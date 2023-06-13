027a13bf0a2ec0848e3a6d6e8e748b9f6c4320c8
Daniel Lima <danielmlimag@gmail.com>
2023-06-13 00:53:38 -0300

    feat: implement post request of signup and login pages

---
+++ pages/sign-up/sign-up.js
@@ -0,0 +1,38 @@
+"use strict";
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get form data
+    const formData = new FormData(this);
+
+    // Convert form data to JSON
+    const jsonData = {};
+    formData.forEach(function (value, key) {
+        jsonData[key] = value;
+    });
+
+    // Make the POST request
+    fetch("https://localhost:7140/api/user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+    })
+        .then(function (response) {
+            if (response.ok) {
+                // Request was successful
+                console.log("Form submitted successfully");
+
+                // Redirect to another page
+                window.location.href = "http://127.0.0.1:5500/index.html";
+            } else {
+                // Request failed
+                console.error("Form submission failed");
+            }
+        })
+        .catch(function (error) {
+            console.error("Error:", error);
+        });
+});
